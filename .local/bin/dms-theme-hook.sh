7fe70df116b597918084f135f9cae5b1998be0a1
ShadowProgr <14125835+ShadowProgr@users.noreply.github.com>
2026-05-04 08:23:33 +0700

    foot: add auto theme switch with system

--- .local/bin/dms-theme-hook.sh
+++ .local/bin/dms-theme-hook.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 MODE=$1
+FOOT_MODE="/home/shadowprogr/.config/foot/current-mode.ini"
 
 if [ "$MODE" = "dark" ]; then
     /home/shadowprogr/.local/bin/pywalfox update
-    # Execute dark mode specific commands
+    echo "initial-color-theme=dark" > "$FOOT_MODE"
+    pkill -USR1 foot
 else
     /home/shadowprogr/.local/bin/pywalfox update
-    # Execute light mode specific commands
+    echo "initial-color-theme=light" > "$FOOT_MODE"
+    pkill -USR2 foot
 fi
